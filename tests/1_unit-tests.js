a187c83a5dffd636304b29b1531aa586b4cff98f
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2021-02-19 22:12:08 -0500

    arrays

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -101,16 +101,16 @@ suite("Unit Tests", function () {
   suite("Arrays", function () {
     // #11
     test("#isArray, #isNotArray", function () {
-      assert.fail(
+      assert.isArray(
         "isThisAnArray?".split(""),
         "String.prototype.split() returns an Array"
       );
-      assert.fail([1, 2, 3].indexOf(2), "indexOf returns a number.");
+      assert.isNotArray([1, 2, 3].indexOf(2), "indexOf returns a number.");
     });
     // #12
     test("Array #include, #notInclude", function () {
-      assert.fail(winterMonths, "jul", "It's summer in july...");
-      assert.fail(
+      assert.notInclude(winterMonths, "jul", "It's summer in july...");
+      assert.include(
         backendLanguages,
         "javascript",
         "JS is a backend language !!"
